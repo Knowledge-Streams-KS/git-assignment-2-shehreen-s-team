85b910c1bfff06074616f2a1e7c4946d41e31370
shehreenFatima <shehreen.fatima07@gmail.com>
2023-01-09 19:46:04 +0500

    mod functionality added in main

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
+import mod;
 def main():
+    value1 = input("enter first value\n")
+    value2 = input("enter second value\n")
+    print(value1, " mod ", value2, "is: ", )
+   
 
 
 main()
--- modulus.py
+++ modulus.py
@@ -1,3 +1,6 @@
+import sys
 def mod(a, b):
     return a%b
 
+
+sys.modules[__name__] = mod
